e70aefa1d2aa6c94a7dd7222a76df77342965f4b
daknob77 <121295353+daknob77@users.noreply.github.com>
2023-03-05 20:57:51 +0800

    Add rspRound function

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
-let choices = ['Rock', 'Paper', 'Scissors']
+let choices = ['rock', 'paper', 'scissors']
+let playerChoice, compChoice;
 
 function getComputerChoice() {
     x = Math.floor(Math.random()*3);
-    y = choices[x];
-    return y;
+    compChoice = choices[x];
+    return compChoice;
+}
+
+function rspRound (compChoice, playerChoice) {
+    x = compChoice
+    y = toLowerCase(playerChoice)
+    
+    if (x == y){
+        return 'It\'s a tie'
+    }
+    else if (x == choices[0] && y == choices[1]) { //rock vs paper
+        return 'Human wins'                         //x == comp y == human
+    }
+    else if (x == choices[0] && y == choices[2]) { //rock vs scissors
+        return 'Computer wins'
+    }
+    else if (x == choices[1] && y == choices[0]) { //paper vs rock
+        return 'Computer wins'
+    }
+    else if (x == choices[1] && y == choices[2]) { //paper vs scissors
+        return 'Human wins'
+    }
+    else if (x == choices[2] && y == choices[0]) { //scissors vs rock
+        return 'Human wins'
+    }
+    else if (x == choices[2] && y == choices[1]) { //scissors vs paper
+        return 'Computer wins'
+    }
 }
