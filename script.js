611be0f9e1eb31b91b6aff0b1b46fea198bda2e2
daknob77 <121295353+daknob77@users.noreply.github.com>
2023-03-12 19:45:07 +0800

    Add Javascript DOM manipulation methods

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 let choices = ['rock', 'paper', 'scissors']
 let playerChoice, compChoice;
-playerChoice = 'rock'
 
 function getComputerChoice() {
     x = Math.floor(Math.random()*3);
@@ -9,35 +8,77 @@ function getComputerChoice() {
 }
 
 function rspRound (compChoice, playerChoice) {
-    x = compChoice
-    y = playerChoice.toLowerCase()
+    x = compChoice;
+    y = playerChoice.toLowerCase();
     
     if (x == y){
-        return 'It\'s a tie'
+        return `It\'s a tie, you both chose ${compChoice}` ;
     }
     else if (x == choices[0] && y == choices[1]) { //rock vs paper
-        return 'Human wins'                         //x == comp y == human
+        humanScore += 1;
+        return `You: ${playerChoice} Computer: ${compChoice} Human wins`; //x == comp y == human
     }
     else if (x == choices[0] && y == choices[2]) { //rock vs scissors
-        return 'Computer wins'
+        computerScore += 1;
+        return `You: ${playerChoice} Computer: ${compChoice} Computer wins`;
     }
     else if (x == choices[1] && y == choices[0]) { //paper vs rock
-        return 'Computer wins'
+        computerScore += 1;
+        return `You: ${playerChoice} Computer: ${compChoice} Computer wins`;
     }
     else if (x == choices[1] && y == choices[2]) { //paper vs scissors
-        return 'Human wins'
+        humanScore += 1;
+        return `You: ${playerChoice} Computer: ${compChoice} Human wins`;
     }
     else if (x == choices[2] && y == choices[0]) { //scissors vs rock
-        return 'Human wins'
+        humanScore += 1;
+        return `You: ${playerChoice} Computer: ${compChoice} Human wins`;
     }
     else if (x == choices[2] && y == choices[1]) { //scissors vs paper
-        return 'Computer wins'
+        computerScore += 1;
+        return `You: ${playerChoice} Computer: ${compChoice} Computer wins`;
     }
 }
 
-function game (){
+/*function game (){
     for (let i = 0; i<5; i++){
         playerChoice = prompt("Choose your fighter")
         alert(rspRound(getComputerChoice(),playerChoice))
     }
+    
 }
+*/
+
+const btn = document.querySelectorAll('button');
+const result = document.querySelector('.result');
+let humanScore = 0;
+let computerScore = 0;
+const score = document.querySelector('.score');
+const winner = document.querySelector('.winner');
+
+btn.forEach((button) => {
+
+    button.addEventListener('click', () => {
+        if (button.id == 'rock'){
+            playerChoice = 'rock';
+            result.textContent = rspRound(getComputerChoice(),playerChoice);
+        } else if (button.id == 'paper'){
+            playerChoice = 'paper';
+            result.textContent = rspRound(getComputerChoice(),playerChoice);
+        } else if (button.id == 'scissors'){
+            playerChoice = 'scissors';
+            result.textContent = rspRound(getComputerChoice(),playerChoice);
+        }
+        if (humanScore == 5){
+            winner.textContent = `Human WINS!`;
+            humanScore = 0;
+            computerScore = 0;
+        } else if (computerScore == 5){
+            winner.textContent = `You DIE!`;
+            humanScore = 0;
+            computerScore = 0;
+        }
+        score.textContent = `Human score: ${humanScore} Computer score: ${computerScore}`;
+    })
+})
+
